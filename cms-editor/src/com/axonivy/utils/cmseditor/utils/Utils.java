ed50fb436d18733783f684cd2be11aa90aebe7af
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2026-02-11 10:41:12 +0700

    MARP-3702 Fix issue from text to html

--- cms-editor/src/com/axonivy/utils/cmseditor/utils/Utils.java
+++ cms-editor/src/com/axonivy/utils/cmseditor/utils/Utils.java
@@ -1,8 +1,6 @@
 package com.axonivy.utils.cmseditor.utils;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -11,29 +9,36 @@ import org.jsoup.parser.Parser;
 
 public class Utils {
 
-  private static final String HTML_TAG_PATTERN = "<.*?>";
   private static final String TABLE_ELEMENT = "table";
   private static final String UNORDERED_PATTERN = "<ul> %s </ul>";
   private static final String LIST_ITEM_PATTERN = "<li style='padding:0 2rem 0.25rem 0;'> %s </li>";
 
   public static String sanitizeContent(String originalContent, String content) {
     var doc = Jsoup.parseBodyFragment(content);
-    if (containsHtmlTag(originalContent)) {
+    if (!isOnlyWrappedPlainText(content)) {
       var originalDoc = Jsoup.parseBodyFragment(originalContent);
       migrateTableAttr(originalDoc, doc);
       doc.outputSettings().escapeMode(EscapeMode.base).prettyPrint(true);
       return Parser.unescapeEntities(doc.body().html(), false);
-    } else {
-      return doc.body().text();
     }
+    return doc.body().text();
   }
 
-  public static boolean containsHtmlTag(String str) {
-    if (Objects.isNull(str)) {
+  public static boolean isOnlyWrappedPlainText(String html) {
+    if (html == null || html.isBlank()) {
+      return true;
+    }
+    Document doc = Jsoup.parseBodyFragment(html);
+    if (doc.body().childrenSize() != 1) {
       return false;
     }
-    var pattern = Pattern.compile(HTML_TAG_PATTERN);
-    return pattern.matcher(str).find();
+    var element = doc.body().child(0);
+    if (!"p".equals(element.tagName())) {
+      return false;
+    }
+    // Allow only text nodes and <br>
+    return element.childNodes().stream()
+        .allMatch(node -> node.nodeName().equals("#text") || node.nodeName().equals("br"));
   }
 
   private static void migrateTableAttr(Document originalDoc, Document doc) {
